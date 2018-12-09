956a861c96581fa70de5d4c60c2f9c10a52615b0
priyaprafful <priyankamaheshwari.2021@gmail.com>
2018-12-09 18:19:04 +0100

    show cart part1

--- routes/cart-router.js
+++ routes/cart-router.js
@@ -6,9 +6,7 @@ const router = express.Router();
 
 router.post("/addtocart", (req,res,next)=>{
     console.log("req.params   ::::::",req.body);
-    const {key,price}  = req.body;
-    console.log("key is :::",key);
-    console.log("loggedInUser :::",req.user);
+    const {key,name,image,price}  = req.body;
     const user = req.user;
     console.log("here we start............")
     if(user!=undefined){
@@ -17,7 +15,7 @@ router.post("/addtocart", (req,res,next)=>{
             console.log("cart found");
             if(cartEntry){
                 console.log("cart is",cartEntry);
-                cartEntry.products.push(key); 
+                cartEntry.products.push(key+"|"+name+"|"+image+"|"+price); 
                 cartEntry.cartTotal = cartEntry.cartTotal +price;              
                 console.log("cart is",cartEntry);
             }
@@ -56,39 +54,64 @@ router.post("/addtocart", (req,res,next)=>{
 
 
 router.post("/myproducts",(req,res,next)=>{
-    //console.log("inside myproducts", req.body);
-    Carts.findOne({user:{$eq:req.user._id}})
-      .then(cart=>{
-        var numberOfProducts=cart.products.length;
-        console.log("numberOfProduct is :::",numberOfProducts);
-        //res.locals.numberOfProducts = numberOfProducts;
-        res.json({"numbers":numberOfProducts});
-      })
-      .catch(err=>next(err));
-})
-router.post("/cartitems",(req,res,next)=>{
+    console.log("inside myproducts", req.user);
     if(req.user!=undefined){
-        console.log("inside cart items in cart router", req.user);
         Carts.findOne({user:{$eq:req.user._id}})
-            .then(cart=>{
-                var products = cart.products;
-                console.log(products);
-                var productsArray = [];
-                var productcount = 0;
-                products.forEach(product => {
-                    productcount++;
-                    console.log(productcount);
-                    Products.findById(product).then(productDoc=>{
-                        console.log("----------------------product ", productcount);
-                        //console.log(productDoc);
-                        productsArray.push(productDoc);                
-                        //console.log("productsJson till now ", productsJson);                    
-                    }).catch(err=>next(err));                
-                });
-                console.log("----------------------------------ENd of for each loop of finding products in cart items----------------");                
-                console.log("productsArray    ::::::::",productsArray );
-                res.json({"products":productsArray});
-        }).catch(err=>next(err));
+        .then(cart=>{
+            var numberOfProducts=cart.products.length;
+            var cartProducts = cart.products;
+            console.log("cart products----------------------------------", cartProducts);
+
+            console.log("numberOfProduct is :::---------------------------",numberOfProducts);
+            //res.locals.numberOfProducts = numberOfProducts;
+
+            var jsonStr = '{"Products":[]}';
+            var jsonObj = JSON.parse(jsonStr);
+
+            cartProducts.forEach(product=>{
+                var id  = product.split('|')[0];
+                console.log("id ::",id);
+                var name = product.split('|')[1];
+                console.log("name:: ",name);
+                var imagePath = product.split('|')[2];
+                console.log("imagePath ::",imagePath);
+                var price = product.split('|')[3];
+                console.log("price ::",price);
+                
+                jsonObj['Products'].push({"id":id,"name":name, "imagePath":imagePath,"price":price});
+                console.log("json object after iteration ::::", jsonObj);
+            });
+
+            var jsonStr = JSON.stringify(jsonObj);
+            console.log("json str   :::: ",jsonStr);
+            res.json({"numbers":numberOfProducts, "products":jsonStr });
+        })
+        .catch(err=>next(err));
     }
 })
+// router.post("/cartitems",(req,res,next)=>{
+//     if(req.user!=undefined){
+//         console.log("inside cart items in cart router", req.user);
+//         Carts.findOne({user:{$eq:req.user._id}})
+//             .then(cart=>{
+//                 var products = cart.products;
+//                 console.log(products);
+//                 var productsArray = [];
+//                 var productcount = 0;
+//                 products.forEach(product => {
+//                     productcount++;
+//                     console.log(productcount);
+//                     Products.findById(product).then(productDoc=>{
+//                         console.log("----------------------product ", productcount);
+//                         //console.log(productDoc);
+//                         productsArray.push(productDoc);                
+//                         //console.log("productsJson till now ", productsJson);                    
+//                     }).catch(err=>next(err));                
+//                 });
+//                 console.log("----------------------------------ENd of for each loop of finding products in cart items----------------");                
+//                 console.log("productsArray    ::::::::",productsArray );
+//                 res.json({"products":productsArray});
+//         }).catch(err=>next(err));
+//     }
+// })
 module.exports= router;
