cbd466846e592fc2d51c96f99bcd27b09271ac43
Harnitks <harnitks@gmail.com>
2018-12-12 19:37:43 +0100

    all problemssss fixes by Nizar

--- bin/productSeed.js
+++ bin/productSeed.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 const Products = require("../models/product-model.js");
---
+++ public/precache-manifest.d627018a763c8d0201021064cff73069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fcff59d1408979ec05db",
+    "url": "/static/js/main.fcff59d1.chunk.js"
+  },
+  {
+    "revision": "fd1596c44f4b7aa43fc5",
+    "url": "/static/js/1.fd1596c4.chunk.js"
+  },
+  {
+    "revision": "fcff59d1408979ec05db",
+    "url": "/static/css/main.ba0c3a6d.chunk.css"
+  },
+  {
+    "revision": "fd1596c44f4b7aa43fc5",
+    "url": "/static/css/1.568beaba.chunk.css"
+  },
+  {
+    "revision": "873ee2e46bfbbde23ef2352120b89dd0",
+    "url": "/index.html"
+  }
+];
